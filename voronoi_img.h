a6f51fa31889069694eed8365580c58687cdc73a
John Doe <johndoe@example.com~>
2023-10-10 15:59:31 +0800

    add show center dot.

--- voronoi_img.h
+++ voronoi_img.h
@@ -16,13 +16,18 @@ inline Img generate_voronoi_img(int px, uint64_t grid) {
     // std::cout << d <<"\n";
     // d = std::max<float>(0,1-d);
 
-    d = std::min<float>(1,d);
+    d = std::min<float>(1, d);
+    // show center dot.
+    
+    // if (d < 0.05) {
+    //   d = 1 - d;
+    // }
     // d = sqrt(d);
-    d = pow(d,2);
+    d = pow(d, 2);
 
     uint8_t color[1];
-    color[0] = d*255.99;
-    info(std::span{(char*)color,1});
+    color[0] = d * 255.99;
+    info(std::span{(char *)color, 1});
   });
   return img;
 }
